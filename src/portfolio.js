dee2efb9bfb697dfdff7b0324a0fd53f175e6adc
GoncalojmRosa <gongarosa12@gmail.com>
2022-02-02 15:18:33 +0000

    First Commit

---
+++ src/portfolio.js
@@ -0,0 +1,387 @@
+
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Gonçalo",
+  logo_name: "goncalo.R()",
+  nickname: "harry / picleric",
+  full_name: "Gonçalo Rosa",
+  subTitle:
+    "Full Stack Developer, Open Source and Cyber Security Enthusiast 🔥. Always learning.",
+  resumeLink:
+    "https://docs.google.com/document/d/1A_jWRG74Rst427tg1izLa6vRXclS9_9F856jWJPJlDY/edit?usp=sharing",
+  mail: "goncalojmrosa@gmail.com",
+};
+
+const socialMediaLinks = {
+  github: "https://github.com/GoncalojmRosa",
+  linkedin: "https://www.linkedin.com/in/gon%C3%A7alo-rosa-8a7a5b1a7/",
+  gmail: "goncalojmrosa@gmail.com",
+
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS, NextJS",
+        "⚡ Creating application backend in Node & Express",
+        "⚡ Integration of third party services such as Firebase/ Heroku / Netifly"
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C#",
+          fontAwesomeClassname: "simple-icons:csharp",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Instituto Politécnico de Castelo Branco",
+      subtitle: "Degree in Computer Engineering",
+      logo_path: "ipcb.png",
+      alt_name: "SSEC",
+      duration: "2021 - Present",
+      descriptions: [
+        "⚡ I'm currently pursuing my Degree in Computer Engineering."
+        // "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        // "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        // "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "https://www.ipcb.pt/",
+    }
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Unity Game Development",
+      subtitle: "ANPRI",
+      logo_path: "unity.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1cqLhz2sMYXPl447MgYKMIg9c83l1cFJJ/view?usp=sharing",
+      alt_name: "Unity",
+      color_code: "#EFF2F1",
+    },
+    {
+      title: "Web Applications with PHP",
+      subtitle: "ANPRI",
+      logo_path: "php.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1fLiR2a5BJNfn1xvBjcjeJWPj2Q_adPAF/view?usp=sharing",
+      alt_name: "PHP",
+      color_code: "#474A8A",
+    },
+    {
+      title: "ReactJS Bootcamp (NLW)",
+      subtitle: "Rocketseat",
+      logo_path: "react.png",
+      alt_name: "PHP",
+      color_code: "#2AAFED",
+    }
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work and Internship ",
+  description:
+    "I've completed two internships. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Game Development, Unity 3D(Intern)",
+          company: "ANPRI",
+          company_url: "https://www.anpri.pt/",
+          logo_path: "anpri.png",
+          duration: "May 2021 - Jul 2021",
+          location: "Work from Home",
+          description:
+          `Train, manage and provide guidance to junior software development staff.
+          Work closely with the Project Manager and Team Leads on change request functions.
+          Develops new and maintains existing applications.
+          Increased productivity and problem-solving technics by 20%.
+          We primarily used PERN and MERN Stack. 80% of my work was doing backend development and the remaining 20% was DevOps-related tasks.
+          `,
+          color: "#0071C5",
+        },
+        {
+          title: "Development of PHP Web Applications (Intern)",
+          company: "ANPRI",
+          company_url: "https://www.anpri.pt/",
+          logo_path: "anpri.png",
+          duration: "May 2020 - Jul 2020",
+          location: "Work From Home",
+          description:
+          `Application to manage an E-Commerce.
+          I Primarily used PHP. 50% of my work was doing backend development, others 40% was designing database schemas, and the last 10% was creating the Website Design using only HTML, CSS & JS.
+          `,
+          
+          color: "#ee3c26",
+        }
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "me.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://twitter.com/",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Proffy",
+      url: "https://github.com/harikanani/AutomateInstaPyBot",
+      description: "Plataform where we can find people to teach us some school subjects (for ReactJS & NodeJS learning purposes)",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "TrailerFlix",
+      url: "https://github.com/harikanani/AutomateInstaPyBot",
+      description: "Simple Netflix clone for movies & series trailers",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    }
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
